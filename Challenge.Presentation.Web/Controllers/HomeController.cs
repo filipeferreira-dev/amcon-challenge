0088b6e1c8c6c828b731a4c957883dbf2f09c8ce
Filipe Ferreira <fferreira@softjournua.onmicrosoft.com>
2023-03-22 13:31:44 -0300

    cleanning

--- Challenge.Presentation.Web/Controllers/HomeController.cs
+++ Challenge.Presentation.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Challenge.ApplicationService.Messages.Response;
-using Challenge.ClientConnector.Contracts;
+﻿using Challenge.ClientConnector.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Challenge.Presentation.Web.Controllers
@@ -7,10 +6,6 @@ namespace Challenge.Presentation.Web.Controllers
     public class HomeController : Controller
     {
         IChallengeServiceConnector ChallengeServiceConnector { get; }
-
-        //no mundo real isso seria um servico de cache ou servico com a chamada para list individualmente as trasações de cada lojista
-        //decidi utilizar essa classe estática para ser mais rapido o desenvolvimento
-        static IEnumerable<MerchantResponse> _merchants;
 
         public HomeController(IChallengeServiceConnector challengeServiceConnector)
         {
@@ -21,19 +16,6 @@ namespace Challenge.Presentation.Web.Controllers
         {
             var summary = await ChallengeServiceConnector.GetSummaryAsync();
             return View(summary);
-        }
-
-        public IActionResult Details(long id)
-        {
-            var merchant = _merchants.FirstOrDefault(m => m.Id == id);
-
-            return View(merchant);
-        }
-
-        [HttpGet]
-        public IActionResult Upload()
-        {
-            return View();
         }
     }
 }
